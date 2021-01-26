ed13891cc96321b21162ce733226b66598abf327
tsuru_hlab_osaka <tsuru@hlab.sys.es.osaka-u.ac.jp>
2021-01-26 17:24:31 +0900

    [Tsuru] remove ghost footstep cache. Now the robot walk more safely.

--- src/states/Standing.cpp
+++ src/states/Standing.cpp
@@ -51,6 +51,9 @@ void states::Standing::start()
   auto & supportContact = ctl.supportContact();
   auto & targetContact = ctl.targetContact();
 
+  controller().datastore().remove("Plugin::FSP::Plan");
+  controller().datastore().make<bool>("Plugin::FSP::Request", true);
+
   planChanged_ = false;
   lastInterpolatorIter_ = ctl.planInterpolator.nbIter;
   leftFootRatio_ = ctl.leftFootRatio();
@@ -191,6 +194,20 @@ void states::Standing::checkPlanUpdates()
       const sva::PTransformd & X_0_rf = controller().robot().surfacePose("RightFootCenter");
       ctl.plan.updateInitialTransform(X_0_lf, X_0_rf, 0);
       ctl.plan.rewind();
+ 
+      // TODO : I found that support contact is changing randomly in standing state. To resume walking naturally, please make the contact stable.
+      
+      //LOG_ERROR("current contact : " << ctl.supportContact().surfaceName);
+      
+      // if(ctl.supportContact().surfaceName == "RightFootCenter")
+      // {
+      //   ctl.plan.reset(0); //restart walking on left foot
+      // }
+      // else// if(ctl.supportContact().surfaceName == "LeftFootCenter")
+      // {
+      //   ctl.plan.reset(1); //restart walking on right foot
+      // }
+
       controller().datastore().remove("Plugin::FSP::Plan");
       mc_rtc::log::info("Current LeftFootCenter: {}", X_0_lf.translation().transpose());
       mc_rtc::log::info("Current RightFootCenter: {}", X_0_rf.translation().transpose());
